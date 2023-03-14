82343995c191da97d520d5993213de169a96f9d0
JAlam <JAlam@optum.optumcare.com>
2023-03-14 17:56:27 +0530

    Css changes

--- src/DataTable/FilterComponent.tsx
+++ src/DataTable/FilterComponent.tsx
@@ -2,7 +2,7 @@ import { Button } from "react-bootstrap";
 
 const FilterComponent = (props :any) => (
     <>
-        <div className="search-filter" style={{marginTop:'-70px'}}>
+        <div className="search-filter" style={{marginTop:'-50px'}}>
             <input type="text" className="search-btn" placeholder="Search"  value={props.filterText} onChange={props.onFilter} />               
             <button type="button"  className="btn btn-primary" onClick={props.onClear}>
                   X
