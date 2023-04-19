f012ec3c3cf34a4e20a55884166729928354def8
JAlam <JAlam@optum.optumcare.com>
2023-04-19 17:30:59 +0530

    Columns and Download buttons reshaped

--- src/DataTable/FilterComponent.tsx
+++ src/DataTable/FilterComponent.tsx
@@ -4,7 +4,7 @@ const FilterComponent = (props :any) => (
     <>
         <div className="search-filter" >
             <input type="text" className="search-btn" placeholder="Search"  value={props.filterText} onChange={props.onFilter} />               
-            <button type="button" style={{padding:"3px 6px 3px", marginTop:"-2px"}}  className="btn btn-primary" onClick={props.onClear}>
+            <button type="button" style={{padding:"4px 6px 5px", marginTop:"-2px"}}  className="btn btn-primary" onClick={props.onClear}>
                   X
             </button>
         </div>   
