f8ded7c2f2aa7db1772bd8334be4f923f6707dd8
JAlam <JAlam@optum.optumcare.com>
2023-05-25 16:12:21 +0530

    Sidebar and functionality for holiday report

--- src/SideBar/SideBar.tsx
+++ src/SideBar/SideBar.tsx
@@ -112,7 +112,7 @@ const SideBar = () => {
                                                     <li className="Collapsible-li"><Link to="/Report/AllocationMarketReport" onClick={() => { navigate(APP_ROUTES.ALLOCATIONMARKETRPOT) }}>Allocation By Market </Link></li>
                                                     <li className="Collapsible-li"><Link to="/Report/AllocationResourceReport" onClick={() => { navigate(APP_ROUTES.ALLOCATIONRESOURCERPOT) }}>Allocation By Resource</Link></li>
                                                     <li className="Collapsible-li"><Link to="/Report/AllocationProjectReport" onClick={() => { navigate(APP_ROUTES.ALLOCATIONPROJECTRPOT) }}>Allocation By Project</Link></li>
-                                                    <li className="Collapsible-li"><Link to="/Report/HolidayListReport" onClick={() => { navigate(APP_ROUTES.HOLIDAYLISTRPOT) }}>Holiday And PTO</Link></li>
+                                                    <li className="Collapsible-li"><Link to="/Report/HolidayListReport" onClick={() => { navigate(APP_ROUTES.HOLIDAYLISTRPOT) }}>Holiday List</Link></li>
                                                 </ul>
                                             </Collapsible>
                                         </div>
