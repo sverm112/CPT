36b946e83c0fb725538b3263d38109ce77522945
Jawed Alam <JAlam@optum.optumcare.com>
2023-07-18 08:25:07 -0700

    Reports full screen

--- src/Reports/ProjectReport/ProjectReport.tsx
+++ src/Reports/ProjectReport/ProjectReport.tsx
@@ -5,8 +5,8 @@ const ProjectReport = () => {
   return (
     <div>
       <SideBar></SideBar>
-      <div className="  col-md-12 bg-mainclass" onClick={closeNav}>
-      <iframe title="Allocation By Project" width="1140" height="541.25" src="http://colo-sqlrptqa/reports/powerbi/CPT/Allocation%20By%20Project?rs:embed=true"></iframe>
+      <div className="  col-md-12 bg-mainclass wrap-element" onClick={closeNav}>
+      <iframe title="Allocation By Project" className="wrapped-iframe" src="http://colo-sqlrptqa/reports/powerbi/CPT/Allocation%20By%20Project?rs:embed=true"></iframe>
       </div>
     </div>
   );
--- src/Reports/ResourceReport/ResourceReport.tsx
+++ src/Reports/ResourceReport/ResourceReport.tsx
@@ -8,8 +8,8 @@ const ResourceReport = () => {
     return (
         <div>
             <SideBar></SideBar>
-            <div className="col-md-12 bg-mainclass" onClick={closeNav}>
-            <iframe title="Allocation By Resource" width="1140" height="541.25" src="http://colo-sqlrptqa/reports/powerbi/CPT/Allocation%20By%20Resource?rs:embed=true" ></iframe>
+            <div className="col-md-12 bg-mainclass wrap-element" onClick={closeNav}>
+            <iframe title="Allocation By Resource" className="wrapped-iframe" width="1140" height="541.25" src="http://colo-sqlrptqa/reports/powerbi/CPT/Allocation%20By%20Resource?rs:embed=true" ></iframe>
             </div>
         </div>
     );
