ccfd227fced0e92655f4efbd7a3323c7aaf88935
JAlam <JAlam@optum.optumcare.com>
2024-01-18 15:13:38 +0530

    Non prod url

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -7,7 +7,7 @@ export * as APP_ROUTES from "./routes";
 // export const Base_URL = 'https://localhost:44314/';
 export const Base_URL = 
 'https://cpt-api-dev.hcp.com:448/';
-// 'http://10.147.172.18:9192/';
+// // 'http://10.147.172.18:9192/';
 export const DASHBOARD_REPORT = "http://colo-sqlrptqa/Reports/powerbi/CPT/Dashboard_Dev?rs:embed=true" 
 export const ALLOCATION_BY_MARKET_REPORT = "http://colo-sqlrptqa/Reports/powerbi/CPT/Allocation%20By%20Market_Dev?rs:embed=true"
 export const ALLOCATION_BY_PROJECT_REPORT = "http://colo-sqlrptqa/Reports/powerbi/CPT/Allocation%20By%20Project_Dev?rs:embed=true"
