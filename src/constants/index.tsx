650e6ad3644cf318356eaa9355585babe8c16f95
JAlam <JAlam@optum.optumcare.com>
2024-02-02 23:00:45 +0530

    Non prod UI

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -4,11 +4,11 @@ let protocol = window.location.href.substring(0,5);
 
 export * as APP_ROUTES from "./routes";
 //Non Prod
-export const Base_URL = 'https://localhost:44314/';
+// export const Base_URL = 'https://localhost:44314/';
 
-// export const Base_URL = 
-// 'https://cpt-api-dev.hcp.com:448/';
-// // 'http://10.147.172.18:9192/';
+export const Base_URL = 
+'https://cpt-api-dev.hcp.com:448/';
+//  'http://10.147.172.18:9192/';
 export const DASHBOARD_REPORT = "http://colo-sqlrptqa/Reports/powerbi/CPT/Dashboard_Dev?rs:embed=true" 
 export const ALLOCATION_BY_MARKET_REPORT = "http://colo-sqlrptqa/Reports/powerbi/CPT/Allocation%20By%20Market_Dev?rs:embed=true"
 export const ALLOCATION_BY_PROJECT_REPORT = "http://colo-sqlrptqa/Reports/powerbi/CPT/Allocation%20By%20Project_Dev?rs:embed=true"
